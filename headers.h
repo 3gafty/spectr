5bfc6e954202fd88fe4e93c2100b3b1f992e2617
3gafty <41028994+3gafty@users.noreply.github.com>
2020-02-03 01:37:00 +0300

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <complex>
+#include <exception>
+#include <fstream>
+#include <fftw3.h>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+// compile by the command
+//g++ -lfftw3 -O2 -std=c++17 read_bin_data.cpp -o read_bin_data
