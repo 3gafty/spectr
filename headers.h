f678e3d835bb63e23d38309cd0e0dc0b79816b51
3gafty <41028994+3gafty@users.noreply.github.com>
2020-02-11 08:50:58 +0300

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,3 +1,6 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
 #include <cmath>
 #include <complex>
 #include <exception>
@@ -6,6 +9,8 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+
+#endif // HEADERS_H
 
 // compile by the command
 //g++ -lfftw3 -O2 -std=c++17 read_bin_data.cpp -o read_bin_data
